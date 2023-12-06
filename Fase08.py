7cf7496394e9e6c46bb6100cb51b601d5076e02d
André Luiz <126507217+andreluizhla@users.noreply.github.com>
2023-12-06 15:15:55 -0300

    Update Fase08.py

--- Fase08.py
+++ Fase08.py
@@ -1,4 +1,4 @@
-import math
+from math import pi
 num = int(input('Digite o raio da circunferência: '))
-c = 2 * math.pi * num
+c = 2 * pi * num
 print('A circunferência desse círculo é igual a {:.2f}'.format(c))
