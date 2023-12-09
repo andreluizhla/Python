dc912ca4ce90e137fb51ce13df7e01179be36fb0
André Luiz <126507217+andreluizhla@users.noreply.github.com>
2023-12-09 12:22:26 -0300

    Python

---
+++ Desafio022.py
@@ -0,0 +1,9 @@
+nome = str(input('Digite o seu nome completo: '))
+
+print(nome.upper())
+print(nome.lower())
+
+nome = nome.split()
+print('Seu nome completo tem {} letras'.format(len(''.join(nome))))
+
+print('Seu primeiro nome {}, tem {} letras'.format(nome[0], len(nome[0])))
