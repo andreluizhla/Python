5e031363172c961217961ce8ed518b28c444002c
André Luiz <126507217+andreluizhla@users.noreply.github.com>
2024-05-02 07:53:39 -0300

    Update Desafio082.py

--- Listas/Desafio082.py
+++ Listas/Desafio082.py
@@ -2,15 +2,16 @@ valores = list()
 pares = list()
 impares = list()
 while True:
-    num = int(input('Digite um número: '))
-    valores.append(num)
-    if num % 2 == 0:
-        pares.append(num)
-    else:
-        impares.append(num)
-    opcao = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
-    if opcao == 'N':
+    valores.append(int(input('Digite um número: ')))
+    opcao = str(input('Quer continuar? [S/N] '))
+    if opcao in 'Nn':
         break
+for pos, val in enumerate(valores):
+    if val % 2 == 0:
+        pares.append(val)
+    else:
+        impares.append(val)
+    
 print('-=' * 30)
 print(f'A lista completa é {valores}')
 print(f'A lista de pares é {pares}')
