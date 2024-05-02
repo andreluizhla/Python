21a1235e3e3c5f71d658cebd0b5ef2f6a162b93e
André Luiz <126507217+andreluizhla@users.noreply.github.com>
2024-05-02 15:49:48 -0300

    Update Desafio080.py

--- Listas/Desafio080.py
+++ Listas/Desafio080.py
@@ -1,25 +1,10 @@
 valores = list()
 for c in range(0, 5):
     num = int(input('Digite um valor: '))
-    if c == 0:
+    if c == 0 or num > valores[-1]:
         valores.append(num)
-    elif c == 1:
-        if num > valores[0]:
-            valores.append(num)
-        else:
-            valores.insert(0, num)
-    elif c >= 2:
-        if num < valores[0]:
-            valores.insert(0, num)
-        elif num > valores[-1]:
-            valores.append(num)
-        for n in range(0, len(valores)):
-            if valores[n] < num < valores[n + 1]:
-                valores.insert(n + 1, num)
-    if valores.index(num) == valores[-1]:
         print('Adicionado ao final da lista...')
     else:
-        print(f'Adicionado na posicao {valores.index(num)} da lista...')
-    print(valores)
+        pass
 print('-=' * 30)
 print(f'Os valores digitados em ordem foram {valores}')
