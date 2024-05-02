73d36c4270a8fba21bb96f78fa116886f4b6bfbe
André Luiz <126507217+andreluizhla@users.noreply.github.com>
2024-05-02 07:38:22 -0300

    Update Desafio081.py

--- Listas/Desafio081.py
+++ Listas/Desafio081.py
@@ -1,14 +1,12 @@
 valores = list()
-cont = 0
 while True:
     num = int(input('Digite um número: '))
     valores.append(num)
-    cont += 1
     opcao = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if opcao == 'N':
         break
 valores.sort(reverse=True)
-print(f'Você digitou {cont} números')
+print(f'Você digitou {len(valores)} números')
 print(f'Os em ordem decrescente são: {valores}')
 if 5 in valores:
     print('Você digitou o número 5')
