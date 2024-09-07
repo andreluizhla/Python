6e0a3e3a6e50c811251e1a2ca8ae004fb5423179
andreluizhla <andre.luiz.hla1@gmail.com>
2024-09-07 20:26:17 -0300

    python

--- Listas/Desafio086.py
+++ Listas/Desafio086.py
@@ -1,6 +1,12 @@
-#fileiras
-primeira = list()
-segunda = list()
-terceira = list()
+tabela = [[], [], []]
 
-
+for lin in range(0, 3):
+    for col in range(0, 3):
+        numero = int(input(f'Digite um valor para [{lin}, {col}]: '))
+        tabela[lin].append(numero)
+        
+print('-=' * 15)
+for lin in range(0, 3):
+    for col in range(0, 3):
+        print(f'[ {tabela[lin][col]} ]', end='')
+    print()
