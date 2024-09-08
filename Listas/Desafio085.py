3e5e17a1044b555a66001f7124d2c32cd90b93b6
andreluizhla <andre.luiz.hla1@gmail.com>
2024-09-08 20:26:50 -0300

    python

--- Listas/Desafio085.py
+++ Listas/Desafio085.py
@@ -4,10 +4,10 @@ for c in range(1, 8):
     n = int(input(f'Digite o {c}° valor: '))
     if n % 2 == 0:
         numeros[0].append(n)
-        numeros[0].sort()
     else:
         numeros[1].append(n)
-        numeros[1].sort()
 print('-=-' * 15)
+numeros[0].sort()
+numeros[1].sort()
 print(f'Os valores pares digitados foram: {numeros[0]}')
 print(f'Os valores ímpares digitados foram: {numeros[1]}')
